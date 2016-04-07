dd515f5cdbe93c93ed2c17c6a19f2dcdf71558af
Mihir Singh <git.service@mihirsingh.com>
2016-04-07 17:06:57 -0400

    Created a setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cloudspecs',
+    version='0.0.1',
+    author='Mihir Singh (@citruspi)',
+    author_email='hello@mihirsingh.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+)
