de83345ab0240be15d4b25ad53a2447d6e7265ce
Mihir Singh <git.service@mihirsingh.com>
2016-04-18 14:53:08 -0400

    Added the location of package data files to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from setuptools import setup, find_packages
+import os
+
+
+def load_data_files():
+    data_files = []
+
+    for root, _, files in os.walk('data'):
+        data_files.extend(['{r}/{f}'.format(r=root, f=f) for f in files])
+
+    return [('data', data_files)]
+
 
 setup(
     name='cloudspecs',
@@ -9,4 +20,5 @@ setup(
     zip_safe=False,
     include_package_data=True,
     platforms='any',
+    data_files=load_data_files()
 )
